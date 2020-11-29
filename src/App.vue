4d5d2e69417bd5ab439aeb3ca06a74bb4cfa6a62
Aya Elsayed <45416886+ayaelsayed25@users.noreply.github.com>
2020-11-29 20:10:37 +0200

    commit on 'master'

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,88 @@
 <template>
   <div id="app">
-    <div id="toolBar">
-      <div id="appLogo">
-        <h1><span>i</span>nspire <i class="fa fa-paint-brush"></i></h1>
-        <h4>Best Drawing & Painting App</h4>
-      </div>
-      <div id="newDrawing"><i class="fa fa-plus"></i> New Drawing</div> 
-      <div id="open"><i class="fa fa-folder-open"></i> Open Drawing</div>
-      <div id="saveDrawing"><i class="fa fa-save"></i> Save Drawing
-      <ul id="saveList">
-        <li>Save as XMl file</li>
-        <li>Save as an image</li>
-        <li>Save as JSON file</li>
-      </ul>
-      </div>
-      <div id="edit"><i class="fa fa-edit"></i> Edit
-       <ul id="editList">
-        <li>Undo</li>
-        <li>Redo</li>
-        <li>Copy</li>
-        <li>Paste</li>
-      </ul>
-      </div>
-    </div>
+    <Tools></Tools>
+    <canvas id="canvas"></canvas>
   </div>
 </template>
 
 <script>
+import Tools from './components/Tools.vue'
 export default {
   name: 'App',
   components: {
+    Tools
+  },
+  data() {
+    return {
+      canvas : null,
+      ctx : null,
+      painting: false,
+      x : 0,
+      y : 0,
+      endX : 0,
+      endY : 0,
+      image : null,
+      objects : [],
+      id : 0
+    }
   },
   mounted() {
-    //NAVIGATION BAR SETTINGS : 
-    var saveDrawing = document.getElementById("saveDrawing");
-    var saveList = document.getElementById("saveList");
-    var edit = document.getElementById("edit");
-    var editList = document.getElementById("editList");
-    saveDrawing.addEventListener('mouseover', () =>{
-      saveList.style.display = "block";
+    this.canvas  = document.getElementById("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.canvas.width = window.innerWidth ;
+    this.canvas.height = window.innerHeight - window.innerHeight * 0.164;
+    window.addEventListener('resize', ()=> {
+      this.canvas.width = window.innerWidth ;
+      this.canvas.height = window.innerHeight - window.innerHeight * 0.164;
     });
-    saveDrawing.addEventListener('mouseout', () =>{
-      saveList.style.display = "none";
-    });
-    edit.addEventListener('mouseover', () =>{
-      editList.style.display = "block";
-    });
-    edit.addEventListener('mouseout', () =>{
-      editList.style.display = "none";
-    });
-    //End Navigation Bar
+    this.canvas.addEventListener('mousedown', this.start);
+    this.canvas.addEventListener('mouseup', this.finish);
+    this.canvas.addEventListener('mousemove', this.drawRect);
+    this.canvas.addEventListener('click', this.check);
+  },
+  methods: {
+    start: function(e)
+    {
+      this.painting = true;
+      this.ctx.beginPath();
+      this.ctx.strokeStyle = "green";
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+      this.image = this.ctx.getImageData(0,0, this.canvas.width, this.canvas.height);
+    },
+    drawRect: function(e){
+      if(!this.painting) return;
+      this.ctx.lineWidth = 7;
+      this.endX = e.offsetX - this.x;
+      this.endY = e.offsetY - this.y;
+      this.ctx.putImageData(this.image, 0,0);
+      this.ctx.strokeRect(this.x, this.y, this.endX, this.endY );
+      console.log(e.offsetX);      
+    },
+    finish: function()
+    {
+      this.painting = false;
+      var object = {color : "black", x : this.x, y : this.y, image : this.ctx.getImageData(0,0, this.canvas.width, this.canvas.height), width : this.endX, height : this.endY};
+      this.objects[this.id] = object;
+      this.id++;
+      for(var i=0; i<this.objects.length; i++)
+      {
+        console.log(this.objects[i]);
+      }
+    },
+    getColorIndicesForCoord: function(x, y, width) {
+      var red = y * (width * 4) + x * 4;
+      return [red, red + 1, red + 2, red + 3];
+    },
+    check: function(e){
+      var myImage = this.ctx.getImageData(0,0, this.canvas.width, this.canvas.height);
+      var colors = this.getColorIndicesForCoord(e.offsetX, e.offsetY, this.canvas.width);
+      console.log(colors);
+      console.log(myImage.data[colors[0]]);
+      console.log(myImage.data[colors[1]]);
+      console.log(myImage.data[colors[2]]);
+      console.log(myImage.data[colors[3]]);
+    }
   }
 }
 </script>
@@ -71,67 +104,9 @@ export default {
   padding: 0;
   font-family: 'Open sans', sans-serif;
 }
-/*NAVIGATION BAR STYLING*/
-#toolBar {
-  text-align: left;
-  background-color: black;
-  color: black;
-  font-weight: 600;
-  font-size: 15px;
-  padding: 12px;
-  width: 98.7vw;
-  margin-left: -0.9%;
-  margin-top: -1.3%;
-  color: white;
-  height: 50%;
+#canvas {
+  background-color: #ffffff;
+  margin : 0;
+  margin-left: -0.7%;
 }
-#toolBar div{
-  display: inline-block;
-  margin-right: 35px;
-  margin-left: 10px;
-  font-size: 17px;
-  cursor: pointer;
-  height: 35px;
-}
-#appLogo {
-  display: inline-block;
-}
-#appLogo h1{
-  font-family: 'Jasmina FY';
-  display: inline-block;
-  margin-bottom: 0px;
-  margin-top: 10px;
-  font-size: 40px;
-}
-#toolBar i {
-  color: #DE3163 ;
-}
-#appLogo span {
-  color: #DE3163;
-}
-#appLogo h4 {
-  font-size: 15px;
-  font-family: 'Jasmina FY';
-  margin-top: 5px;
-}
-#saveDrawing ul, #edit ul{
-  position: absolute;
-  list-style-type: none;
-  background-color:rgba(0, 0, 0, 0.616) ;
-  margin-top: 5px;
-  display: none;
-}
-#saveDrawing ul li, #edit ul li {
-  margin-left: -40px;
-  margin-bottom: 5px;
-  height: 40px;
-  width: 160px;
-  padding-left:10px ;
-  padding-top: 10px;
-  cursor: pointer;
-}
-#saveDrawing ul li:hover, #edit ul li:hover {
-  background-color: #DE3163;
-}
-/*END NAVIGATION BAR STYLING*/
 </style>
