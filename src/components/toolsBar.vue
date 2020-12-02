99a029e4e8aa3ec0e402e73e4f27c8b745bd23d9
sarahelshabrawy <hylrcs@gmail.com>
2020-12-02 11:38:18 +0200

    toolsBar merge Final

--- src/components/toolsBar.vue
+++ src/components/toolsBar.vue
@@ -1,26 +1,26 @@
 <template>
-   <div class="grid-container">
-     <button  class="grid-item" id="tools"><span style='color:white;'>Tools</span> </button>
-     
-     <button  class="grid-item" id="select" title="select" >   <i class="material-icons">near_me</i> </button>
-     <button  class="grid-item" id="delete" title="delete" >   <i class="material-icons">delete</i> </button>
-      <button  class="grid-item" id="copy" title="copy" >   <i class="material-icons">content_copy</i> </button>
+  <div class="grid-container">
+    <button  class="grid-item" id="tools"><span style='color:white;'>Tools</span> </button>
+
+    <button   @click="$emit('setselectmode',true)" class="grid-item" id="select" title="select" >   <i class="material-icons">near_me</i> </button>
+    <button  class="grid-item" id="delete" title="delete" >   <i class="material-icons">delete</i> </button>
+    <button  class="grid-item" id="copy" title="copy" >   <i class="material-icons">content_copy</i> </button>
     <button  class="grid-item" id="move" title="move" >   <i class="material-icons">content_cut</i> </button>
      <button  class="grid-item" id="resize" title="resize" >   <i class="material-icons">photo_size_select_small</i> </button>
      <button   class="grid-item" id="brush" title="brush" > <i class="material-icons">brush</i>   </button>
      <button  class="grid-item" id="pen" title="pen" >   <i class="material-icons">create</i> </button>
      <button @click="$emit('setshape','line')" class="grid-item" id="line" title="line" >   <span style='font-size:20px;'>&#9586;</span> </button>
-     <button @click="$emit('setshape','circle')" class="grid-item" id="circle" title="circle" >   <i class="material-icons">panorama_fish_eye</i> </button>   
+     <button @click="$emit('setshape','circle')" class="grid-item" id="circle" title="circle" >   <i class="material-icons">panorama_fish_eye</i> </button>
      <button @click="$emit('setshape','triangle')" class="grid-item" id="triangle" title="triangle" >   <i class="material-icons">change_history</i> </button>
     <button @click="$emit('setshape','rectangle')" class="grid-item" id="rectangle" title="rectangle" >   <span style='font-size:30px;'>&#9645;</span> </button>
     <button @click="$emit('setshape','ellipse')" class="grid-item" id="eclipse" title="eclipse" >   <span style='font-size:30px;'>&#x2B2D;</span> </button>
     <button @click="$emit('setshape','hexagon')" class="grid-item" id="hexagon" title="hexagon" >   <span style='font-size:30px;'>	&#11041;</span> </button>
       <button @click="$emit('setshape','pentagon')" class="grid-item" id="pentagon" title="pentagon" >   <span style='font-size:30px;'>	&#11040;</span> </button>
    <button  class="grid-item" id="format_color_fill" title="format_color_fill" >   <i class="material-icons">format_color_fill</i> </button>
-      
+
    </div>
 
-   
+
 </template>
 
 
@@ -28,7 +28,7 @@
 <script>
 export default {
   name: 'toolsBar',
-  
+
 }
 </script>
 
@@ -49,8 +49,8 @@ export default {
   border: 1px solid white;
   /*height: 40px;*/
   text-align: center;
-  
-  
+
+
 }
 #tools{
   background-color: #595959;
