15bea5d49227f85c5069326982d2a5083cf7c6b3
radwaahmed20112000 <you@example.com>
2021-09-23 16:47:06 +0200

    Initial Commit

--- src/main/java/com/example/mail_server/Model/Filter/FilterField.java
+++
@@ -1,9 +0,0 @@
-package com.example.mail_server.Model.Filter;
-
-import java.util.LinkedList;
-import com.example.mail_server.Model.Mail.indexMail;
-
-public interface FilterField {
-    public LinkedList<indexMail> filter(LinkedList<indexMail> mails);
-    public void setFilter(String subject);
-}
